f57a4648d15f8e9fdb50abdfff131cc9214c78a2
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-13 00:09:21 +0900

    change_column

--- db/migrate/20200812150409_add_column_to_users.rb
+++ db/migrate/20200812150409_add_column_to_users.rb
@@ -4,8 +4,8 @@ class AddColumnToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :name, :string
     add_column :users, :gender, :string
     add_column :users, :user_image, :string
-    add_column :users, :height, :integer
-    add_column :users, :weight, :integer
-    add_column :users, :basal_metabolism, :integer
+    add_column :users, :height, :float
+    add_column :users, :weight, :float
+    add_column :users, :basal_metabolism, :float
   end
 end
---
+++ db/migrate/20200812150905_change_title_to_records.rb
@@ -0,0 +1,4 @@
+class ChangeTitleToRecords < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
