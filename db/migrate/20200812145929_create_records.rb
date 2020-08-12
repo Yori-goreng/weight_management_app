b1996ed79a3df08961f88406372752c13b52916b
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-13 00:01:06 +0900

    CreateRecordsのカラム追加

--- db/migrate/20200812145929_create_records.rb
+++ db/migrate/20200812145929_create_records.rb
@@ -1,6 +1,18 @@
 class CreateRecords < ActiveRecord::Migration[5.2]
   def change
     create_table :records do |t|
+      t.integer :morning_cal
+      t.string :morning_image
+      t.integer :lunch_cal
+      t.string :lunch_image
+      t.integer :dinner_cal
+      t.string :dinner_string
+      t.integer :total_cal
+      t.string :motion
+      t.integer :motion_time
+      t.integer :consumption_cal
+      t.integer :weight
+      t.timestamps
     end
   end
 end
