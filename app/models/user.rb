e5f53c8d2abbbee4285685f63ca603d2bd40070f
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-12 17:51:32 +0900

    has_many :records追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :graphs, dependent: :destroy
+  has_many :records
+  if gender == '男'
+    self.basal_metabolism = ((13.397 * weight) + (4.799 * height) - (5.677 * age) + 88.362).round
+  else
+    self.basal_metabolism = ((9.247 * weight) + (3.098 * height) - (4.33 * age) + 447.593).round
+  end
 end
-.
+end
