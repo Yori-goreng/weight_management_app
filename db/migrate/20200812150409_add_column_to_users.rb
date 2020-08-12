6195c66c5b6764b2fe2a9c0042596d5924a02997
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-13 00:04:33 +0900

    addcolumntousers追加

---
+++ db/migrate/20200812150409_add_column_to_users.rb
@@ -0,0 +1,10 @@
+class AddColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :age, :integer
+    add_column :users, :gender, :string
+    add_column :users, :user_image, :string
+    add_column :users, :height, :integer
+    add_column :users, :weight, :integer
+    add_column :users, :basal_metabolism, :integer
+  end
+end
