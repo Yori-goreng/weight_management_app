17758c03d8428de2002e1361039abdc2b0bf1c3b
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-13 11:51:10 +0900

    development.rb修正

--- vendor/bundle/ruby/2.6.0/gems/bootstrap-4.4.1/test/dummy_rails/config/environments/development.rb
+++ vendor/bundle/ruby/2.6.0/gems/bootstrap-4.4.1/test/dummy_rails/config/environments/development.rb
@@ -16,8 +16,17 @@ Dummy::Application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.active_record.migration_error = :page_load
+  config.active_record.verbose_query_logs = true
+  
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.assets.quiet = true
+
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 end
