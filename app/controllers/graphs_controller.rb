5cb5c47b0bd7b8d68e719489b6bca4dc77f5d12b
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-13 10:49:57 +0900

    praphs_controller追加

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,11 +1,21 @@
 class GraphsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+    @q = Graph.ransack(params[:q])
+    @graphs = @q.result(distinct: true).order(record_date: "DESC")
+
     gon.weight_records = Graph.chart_data(current_user)
     gon.recorded_dates = current_user.graphs.map(&:date)
+    gon.records = Graph.chart_data(User.find_by(id: 1))
+    gon.start_date = gon.records.first[:record_date].strftime('%Y-%m-%d')
+    gon.end_date = gon.records.last[:record_date].strftime('%Y-%m-%d')
+    gon.basal_metabolism = User.find_by(id: 1).basal_metabolism
   end
 
   def create
       @graph = current_user.graphs.build(graph_params)
+      @graph = Graph.new
       date = @graph.date.strftime('%Y/%-m/%-d')
       if @graph.save
         flash[:notice] = "#{date}の記録を追加しました"
@@ -13,6 +23,20 @@ class GraphsController < ApplicationController
         flash[:alert] = 'エラーが発生しました'
       end
       redirect_to root_path
+  end
+
+  def new
+    @graph = Graph.new
+  end
+
+  def create
+    @graph = Graph.new(graph_params)
+    @graph.user_id = current_user.id
+    if @graph.save
+      redirect_to("/graphs")
+    else
+      render(new_graph_path)
+    end
   end
 
   def update
@@ -41,7 +65,10 @@ class GraphsController < ApplicationController
   private
 
   def graph_params
-    params.require(:graph).permit(:date, :weight)
+    params.require(:graph).permit(:date, :record_date, :morning_cal, :morning_image, :lunch_cal, :lunch_image, :dinner_cal, :dinner_image, :motion1, :motion1_hour, :motion1_minute, :consumption1_cal, :motion2, :motion2_hour, :motion2_minute, :consumption2_cal, :motion3, :motion3_hour, :motion3_minute, :consumption3_cal, :weight)
   end
 
+  def params_graph_search
+    params.permit(:search_weight)
+  end
 end
