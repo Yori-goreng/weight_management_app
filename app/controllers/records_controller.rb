5c5f6e9d28647f2f44dca4fdf578d18d658c4461
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-13 10:20:03 +0900

    indexアクション追加

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,6 +2,13 @@ class RecordsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    
+    @q = Record.ransack(params[:q])
+    @records = @q.result(distinct: true).order(record_date: "DESC")
+
+    gon.records = Record.chart_data(User.find_by(id: 1))
+    gon.start_date = gon.records.first[:record_date].strftime('%Y-%m-%d')
+    gon.end_date = gon.records.last[:record_date].strftime('%Y-%m-%d')
+    gon.basal_metabolism = User.find_by(id: 1).basal_metabolism
   end
+  
 end
